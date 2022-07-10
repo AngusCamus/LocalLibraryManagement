109effc1fd6ed0618b3c808003236c7d0024245a
Brian Polesel <brianpolesel07@gmail.com>
2022-07-10 17:01:53 -0300

    Cambio ALLOWED_HOSTS para agregar nuevo dominio

--- biblioteca_local/settings.py
+++ biblioteca_local/settings.py
@@ -29,7 +29,7 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
-    'dry-journey-94644.herokuapp.com',
+    'local-library-pole.herokuapp.com',
     '127.0.0.1'
 ]
 
